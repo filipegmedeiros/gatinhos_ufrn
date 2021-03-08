96b112a02e4f8aa6fc0376359573a28e2970253f
Karine <karinepcc@gmail.com>
2021-03-08 14:31:26 -0300

    Troca da visibilidade da senha implementada

--- mobile/gatinhos_mobile/lib/ui/login.dart
+++ mobile/gatinhos_mobile/lib/ui/login.dart
@@ -11,6 +11,14 @@ class _LoginState extends State<Login> {
 
   GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
 
+  // Alternar visibilidade da senha
+  bool _hidePassword = true;
+  void _toggleVisibilityPasswd() {
+    setState(() {
+      _hidePassword = !_hidePassword;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,7 +52,7 @@ class _LoginState extends State<Login> {
                     filled: true,
                     fillColor: Color(0xffb9ecf2),
                   ),
-                  textAlign: TextAlign.right,
+                  textAlign: TextAlign.left,
                   style: TextStyle(
                     color: Colors.black,
                     fontSize: 25,
@@ -57,18 +65,27 @@ class _LoginState extends State<Login> {
                 Divider(),
                 TextFormField(
                   keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
+                  obscureText: _hidePassword,
                   decoration: InputDecoration(
-                    labelText: "Senha",
-                    labelStyle: TextStyle(
-                      color: Colors.grey[700],
-                      fontSize: 20,
-                    ),
-                    border: const UnderlineInputBorder(),
-                    filled: true,
-                    fillColor: Color(0xffb9ecf2),
-                  ),
-                  textAlign: TextAlign.right,
+                      labelText: "Senha",
+                      labelStyle: TextStyle(
+                        color: Colors.grey[700],
+                        fontSize: 20,
+                      ),
+                      border: const UnderlineInputBorder(),
+                      filled: true,
+                      fillColor: Color(0xffb9ecf2),
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          _toggleVisibilityPasswd();
+                        },
+                        child: Icon(
+                            _hidePassword
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                            color: Colors.grey),
+                      )),
+                  textAlign: TextAlign.left,
                   style: TextStyle(
                     color: Colors.black,
                     fontSize: 25,
