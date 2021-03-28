ece38bf6d32c41f90abc0bc7c006603533651753
Sara Beatriz Mendonça de Santana <sarabeatriz@ufrn.edu.br>
2021-03-28 18:04:02 -0300

    Changes the card layout adding title and subtitle

--- mobile/gatinhos_mobile/lib/ui/adoptionRequests.dart
+++ mobile/gatinhos_mobile/lib/ui/adoptionRequests.dart
@@ -20,18 +20,40 @@ class _AdoptionRequestsState extends State<AdoptionRequests> {
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  Container(
-                      width: 120,
-                      height: 140,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage('images/cat1.jpg')),
-                      )),
-                  TextButton(
-                    child: const Text('Ver mais'),
-                    onPressed: () {/* ... */},
-                  )
+                  Expanded(
+                      flex: 3,
+                      child: Container(
+                          width: 120,
+                          height: 140,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage('images/cat1.jpg')),
+                          ))),
+                  Expanded(
+                    flex: 7,
+                    child: Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          children: [
+                            Row(children: <Widget>[
+                              Text("Pedido de adoção #1",
+                                  style: TextStyle(fontSize: 18)),
+                            ]),
+                            SizedBox(height: 8),
+                            Row(
+                              children: [
+                                Text("Feito por Sara Beatriz",
+                                    style: TextStyle(
+                                        color: Colors.grey[800], fontSize: 12))
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -39,14 +61,40 @@ class _AdoptionRequestsState extends State<AdoptionRequests> {
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  Container(
-                      width: 120,
-                      height: 140,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage('images/cat2.jpg')),
-                      )),
+                  Expanded(
+                      flex: 3,
+                      child: Container(
+                          width: 120,
+                          height: 140,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage('images/cat2.jpg')),
+                          ))),
+                  Expanded(
+                    flex: 7,
+                    child: Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          children: [
+                            Row(children: <Widget>[
+                              Text("Pedido de adoção #2",
+                                  style: TextStyle(fontSize: 18)),
+                            ]),
+                            SizedBox(height: 8),
+                            Row(
+                              children: [
+                                Text("Feito por Karine",
+                                    style: TextStyle(
+                                        color: Colors.grey[800], fontSize: 12))
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
