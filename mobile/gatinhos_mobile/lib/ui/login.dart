a29281d61138bb3d4aec29abf9e2989f50e0f507
Karine <karinepcc@gmail.com>
2021-04-01 23:10:48 -0300

    login page: add login button; refactor input field wodgets; saved login fields;

--- mobile/gatinhos_mobile/lib/ui/login.dart
+++ mobile/gatinhos_mobile/lib/ui/login.dart
@@ -11,6 +11,8 @@ class _LoginState extends State<Login> {
 
   GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
 
+  String userName, password;
+
   // Alternar visibilidade da senha
   bool _hidePassword = true;
   void _toggleVisibilityPasswd() {
@@ -39,67 +41,119 @@ class _LoginState extends State<Login> {
                 Image.asset(
                   "images/LogoGatinhos.png",
                   fit: BoxFit.fitWidth,
-                  height: 200.0,
+                  height: 220.0,
                 ),
+                Divider(
+                  height: 55,
+                ),
+                _inputUserName(),
                 Divider(),
-                TextFormField(
-                  keyboardType: TextInputType.name, // nome ou email ???
-                  decoration: InputDecoration(
-                    labelText: "Usuário",
-                    labelStyle:
-                        TextStyle(color: Colors.grey[700], fontSize: 20),
-                    border: const UnderlineInputBorder(),
-                    filled: true,
-                    fillColor: Color(0xffb9ecf2),
-                  ),
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 25,
-                  ),
-                  controller: usuarioController,
-                  validator: (value) {
-                    if (value.isEmpty) return "Isira o seu nome de usuário!";
-                  },
-                ),
+                _inputPassword(),
                 Divider(),
-                TextFormField(
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: _hidePassword,
-                  decoration: InputDecoration(
-                      labelText: "Senha",
-                      labelStyle: TextStyle(
-                        color: Colors.grey[700],
-                        fontSize: 20,
-                      ),
-                      border: const UnderlineInputBorder(),
-                      filled: true,
-                      fillColor: Color(0xffb9ecf2),
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          _toggleVisibilityPasswd();
-                        },
-                        child: Icon(
-                            _hidePassword
-                                ? Icons.visibility_off
-                                : Icons.visibility,
-                            color: Colors.grey),
-                      )),
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 25,
-                  ),
-                  controller: senhaController,
-                  validator: (value) {
-                    if (value.isEmpty) return "Isira sua senha!";
-                  },
-                ),
+                ElevatedButton(
+                    onPressed: _sendToServer,
+                    child: Text(
+                      'LOGIN',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xff6200ee),
+                      padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                    )),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  /// form field widgets
+  _inputUserName() {
+    return TextFormField(
+      keyboardType: TextInputType.name, // nome ou email ???
+      decoration: InputDecoration(
+        labelText: "Usuário",
+        labelStyle: TextStyle(color: Colors.grey[700], fontSize: 20),
+        border: const UnderlineInputBorder(),
+        filled: true,
+        fillColor: Color(0xffb9ecf2),
+      ),
+      textAlign: TextAlign.left,
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 25,
+      ),
+      controller: usuarioController,
+      validator: validateUser,
+      onSaved: (str) {
+        userName = str;
+      },
+    );
+  }
+
+  _inputPassword() {
+    return TextFormField(
+      keyboardType: TextInputType.visiblePassword,
+      obscureText: _hidePassword,
+      decoration: InputDecoration(
+          labelText: "Senha",
+          labelStyle: TextStyle(
+            color: Colors.grey[700],
+            fontSize: 20,
+          ),
+          border: const UnderlineInputBorder(),
+          filled: true,
+          fillColor: Color(0xffb9ecf2),
+          suffixIcon: GestureDetector(
+            onTap: () {
+              _toggleVisibilityPasswd();
+            },
+            child: Icon(_hidePassword ? Icons.visibility_off : Icons.visibility,
+                color: Colors.grey),
+          )),
+      textAlign: TextAlign.left,
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 25,
+      ),
+      controller: senhaController,
+      validator: validatePassword,
+      onSaved: (str) {
+        password = str;
+      },
+    );
+  }
+
+  /// Field validators
+  String validateUser(String value) {
+    if (value == null || value.isEmpty) {
+      return "Por favor, insira o seu nome de usuário.";
+    }
+    return null;
+  }
+
+  String validatePassword(String value) {
+    if (value == null || value.isEmpty) {
+      return "Por favor, insira sua senha.";
+    } else if (value.length < 4) {
+      return "Senha deve ter pelo menos 4 caracteres.";
+    }
+    return null;
+  }
+
+  /// submit button onPressed function
+  void _sendToServer() {
+    // Validate returns true if the form is valid, or false otherwise
+    if (_formKey.currentState.validate()) {
+      // If the form is valid, display a status snackbar
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Verificando login...')));
+
+      // save login
+      _formKey.currentState.save();
+    }
   }
 }
